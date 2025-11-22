68b963bb4ef07cd8f4394598d8f50b2abb1e888a
Oscar Alpstig <oalpstig@hotmail.com>
2025-11-22 01:35:54 +0100

    Date format fix

--- src/dateUtils.ts
+++ src/dateUtils.ts
@@ -1,6 +1,6 @@
 export function getSeasonDate(month: number, day: number, nextYear: boolean = false): Date {
   const now = new Date();
   const year = nextYear ? now.getFullYear() + 1 : now.getFullYear();
-  return new Date(year, month, day);
+  return new Date(year, month - 1, day);
 }
 
